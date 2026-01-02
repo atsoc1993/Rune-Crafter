850d856cdfb8a23e90877f2ecfa77d2f9c4cbacb
atsoc1993 <atsoc1993@gmail.com>
2026-01-02 14:09:21 -0500

    Removed a layer of framing borders

--- runecrafter/src/components/MainFrame.tsx
+++ runecrafter/src/components/MainFrame.tsx
@@ -13,19 +13,17 @@ export default function Frame({ tabs }: Props) {
       <div className='h-full w-full bg-teal-800 shadow-2xl shadow-teal-800 p-6 rounded-2xl'>
         <div className='h-full w-full bg-teal-700 shadow-2xl shadow-teal-700 p-6 rounded-2xl'>
           <div className='h-full w-full bg-teal-600 shadow-2xl shadow-teal-600 p-6 rounded-2xl'>
-            <div className='h-full w-full bg-teal-500 shadow-2xl shadow-teal-500 p-6 rounded-2xl'>
               <div className='h-1/6 w-full flex justify-evenly bg-teal-400 rounded-2xl'>
                 {tabs.map(tab => (
                   <div key={tab.name}
                     onClick={() => setTabSelected(tab.name)}
-                    className={'h-full w-1/6 text-center content-center rounded-2xl'
+                    className={'h-full w-2/6 text-center content-center rounded-2xl'
                       + (tabSelected === tab.name ? ' bg-red-200 text-white' : '')}>{tab.name}
                   </div>
                 ))}
               </div>
               {tabContentElement}
             </div>
-          </div>
         </div>
       </div>
     </div>
