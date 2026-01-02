f88e8f732f4d215931d220a49250b46b4b88f8a1
atsoc1993 <atsoc1993@gmail.com>
2026-01-02 13:08:47 -0500

    Accidentally moved progress bar into essence count circle, actually looks way cooler

--- runecrafter/src/components/RuneMiningComponent.tsx
+++ runecrafter/src/components/RuneMiningComponent.tsx
@@ -28,27 +28,28 @@ export default function RuneMining() {
             if (!maxBarWidth) return;
             const newWidth = maxBarWidth * (miningProgress / 100);
             progressBar.current.style.width = newWidth + 'px';
-        }, 100)
+        }, 10)
 
         return () => clearInterval(interval)
     }, [miningProgress, miningLevel])
 
     return (
-        <div className="h-full w-full flex flex-col justify-evenly p-10">
-            <div className="h-1/4 w-3/4 bg-white inset-shadow-sm inset-shadow-black rounded-2xl place-self-center"
-                ref={el => setMaxBarWidth(el?.getBoundingClientRect().width)}>
-                <div className="h-full w-0 bg-green-400 rounded-2xl shadow-sm shadow-black transition-[width] duration-100"
-                    ref={progressBar => {
-                        if (!progressBar) return;
-                        if (!maxBarWidth) return;
-                        const newWidth = maxBarWidth * (miningProgress / 100);
-                        progressBar.style.width = newWidth + 'px';
-                    }}>
+        <div className="h-5/6 w-full flex flex-col justify-evenly">
+            <h1 className="text-center text-5xl">Mining Level: {miningLevel}</h1>
+            <div className="h-100 w-100 bg-gray-300 inset-shadow-sm inset-shadow-gray-500 rounded-full text-2xl px-10 place-self-center text-center flex flex-col justify-evenly">Essence: {essenceCount}
+                <div className="h-1/4 w-3/4 bg-white inset-shadow-sm inset-shadow-black rounded-2xl place-self-center"
+                    ref={el => setMaxBarWidth(el?.getBoundingClientRect().width)}>
+                    <div className="h-full w-0 bg-green-400 rounded-2xl shadow-sm shadow-black transition-[width] duration-100"
+                        ref={progressBar => {
+                            if (!progressBar) return;
+                            if (!maxBarWidth) return;
+                            const newWidth = maxBarWidth * (miningProgress / 100);
+                            progressBar.style.width = newWidth + 'px';
+                        }}>
+
+                    </div>
 
                 </div>
-
-            </div>
-            <div className="h-1/4 w-fit bg-gray-300 inset-shadow-sm inset-shadow-gray-500 rounded-2xl text-4xl px-10 place-self-center text-center content-center">Essence: {essenceCount}
             </div>
         </div>
     )
