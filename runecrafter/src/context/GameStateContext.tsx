58d8d15d47d1e27e4e5f0e7120e446d230d2a1b8
atsoc1993 <atsoc1993@gmail.com>
2026-01-02 14:10:39 -0500

    miningProgress is now a useRef, removed explicitly stated key names and used generic typing for keys & values

--- runecrafter/src/context/GameStateContext.tsx
+++ runecrafter/src/context/GameStateContext.tsx
@@ -1,16 +1,10 @@
 import { createContext, type SetStateAction } from "react";
 
-export type RuneSupply = {
-    Air: number
-    Mind: number
-    Water: number
-    Earth: number
-    Fire: number
-};
+export type RuneSupply = { [key: string]: number };
 
 export type GameState = {
     miningLevelState: [number, React.Dispatch<SetStateAction<number>>]
-    miningProgress: [number, React.Dispatch<SetStateAction<number>>]
+    miningProgress:  React.RefObject<number>
     essenceCount: [number, React.Dispatch<SetStateAction<number>>]
     runeSupplyState: [RuneSupply, React.Dispatch<SetStateAction<RuneSupply>>]
 };
