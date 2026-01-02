c313a66aeb58d7504bf5ce367547d8c3b5a95ac2
atsoc1993 <atsoc1993@gmail.com>
2026-01-02 13:09:03 -0500

    Full rounded the rune count divs

--- runecrafter/src/components/RunesComponent.tsx
+++ runecrafter/src/components/RunesComponent.tsx
@@ -19,7 +19,7 @@ export default function Runes() {
     return (
         <div className='h-5/6 w-full p-6 flex justify-evenly flex-row text-center items-center '>
             {Object.entries(runesSupply).map((rune) => (
-                <div className={'h-1/4 w-1/8 rounded-2xl content-center bg-white shadow-md shadow-teal-600 inset-shadow-sm ' + (insetShadowColorMap[rune[0]])}>
+                <div className={'h-50 w-50 rounded-full content-center bg-white shadow-md shadow-teal-600 inset-shadow-sm text-3xl ' + (insetShadowColorMap[rune[0]])}>
                     <p>{rune[0]}: {rune[1]}</p>
                 </div>
             ))}
