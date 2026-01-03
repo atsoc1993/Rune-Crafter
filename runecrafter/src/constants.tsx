3319622f44605fbe918915e38010850a29f482e4
atsoc1993 <atsoc1993@gmail.com>
2026-01-02 21:36:34 -0500

    Created constants.tsx and included RuneLevelRequirements

---
+++ runecrafter/src/constants.tsx
@@ -0,0 +1,9 @@
+export type RuneLevelRequirements = { [key: string]: number }
+
+export const RuneLevels: RuneLevelRequirements = {
+    Air: 1,
+    Mind: 1,
+    Water: 5,
+    Earth: 15,
+    Fire: 30
+}
